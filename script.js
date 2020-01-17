26cb927eb4291784d06b1cbab290b8b6a1767fff
DenAustin <uduogu@gmail.com>
2020-01-17 17:38:24 +0200

    added copy function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //document.getElementById("start").addEventListener("click", startFlashing) //flash
 // Event listener-function to call generateColors function
-/*
+
 let halt; // to store or return the id of the generator's setInterval function
 document.getElementById("generate").addEventListener("click", () => {
-  generateColors();
+  halt = generateColors();
 });
-*/
+
+document.getElementById("reset").addEventListener("click", () => {
+  clearInterval(halt);
+});
+
+//onclick="generateColors()"
 
 let clear; // to store or return the id of the setInterval function
 document.getElementById("start").addEventListener("click", () => {
@@ -16,6 +21,15 @@ document.getElementById("stop").addEventListener("click", () => {
   clearInterval(clear);
 });
 
+// *** *** COPY FUNCTIONN *** COPY FUNCTION ***********************************
+const copyFunction=(element)=> {
+  let fakeTextArea = document.createElement("textarea");
+  fakeTextArea.value = element.textContent; //element is the js variable which content to be copied
+  document.body.appendChild(fakeTextArea);
+  fakeTextArea.select();
+  document.execCommand("Copy");
+  fakeTextArea.remove()
+}
 
 
 /* document.body.append(div); */
@@ -29,7 +43,7 @@ const generateHexaColor = () => {
   }
   return hexaColor
 }
-console.log(generateHexaColor());
+
 
 /* let userValue = document.getElementById("field")
 let valueNum = userValue.val */
@@ -49,15 +63,20 @@ function generateColors() {
     let copyButon = document.createElement("button"); // copy button created
     copyButon.className ="copyButton"
     copyButon.textContent = "copy"
+    copyButon.addEventListener ("click", ()=>  copyFunction(colorCodeText)); //
     copyDiv.append(copyButon); //container for hexacolor copy button 
     
-    //document.getElementById("button").addEventListener("click", clearContainer) // clear all
+     // clear all
     //document.getElementById("stop").addEventListener("click", flashing) // stop flashing
     
     
     let copyTextDiv = document.createElement("div"); // div to contain copy paragraph
     copyTextDiv.className ="copyIcon" //class name for copy icon
+    console.log("XXZZXX")
+    console.log(copyTextDiv)
     let colorCodeText = document.createElement("p"); // paragraph for hexacolor code
+    console.log("PPP")
+    console.log(colorCodeText)
     colorCodeText.className ="copyCodeParagraph" //class name for generated color
     colorCodeText.textContent = generateHexaColor();
     copyTextDiv.append(colorCodeText); // container for Hexacolor code * going to wrap
@@ -84,12 +103,14 @@ let coloBlocks = document.getElementsByClassName("colorBlock")
     for(let block of coloBlocks)
     block.style.background = generateHexaColor();
   }
-   
+//document.getElementById("reset").addEventListener("click", clearAll) // To clear all
 //document.getElementById("reset").addEventListener("click", clearContainer) // clear all
 //fuction to stop flashing
+/*
 let flashStopper = () => {
   clearInterval(flashing);
 }
+*/
 /*
  // flashing interval setting (w3school variant)
  myInterval = setInterval(flashing, 1000)
@@ -118,7 +139,7 @@ let stopAction  = () => {
 
   } 
 
-// To clear flash
+
 let clearContainer = () => {
   //let colorContainer = document.querySelector("#container");
   //block.textContent = "";
@@ -155,21 +176,31 @@ let blocksGenerator = () => {
   // for start button
  
 
-/*stopAction = document.getElementById("button");
-let clear = document.getElementById("inputField")
-let clearField = clear.value
-  stopAction  = () => {
-      clearField.innerHTML = "";
-  } 
-stopAction()
+let clearElement = document.getElementById("reset");
+let wipeDisplay = clearElement.value;
 /*
-let clear = document.querySelector("inputField");
-  console.log(clear);
-    clear.onclick = function () {
-      clear.innerHTML = "";
+let clearAll = () => {
+  let wipe = document.getElementById("inputField")
+  let clearInput = wipe.value
+  console.log(clearInput)
+  clearInput.value= ""
+ 
+  } 
+
+ 
+
+
+/*        
+let erase = document.querySelector("inputField");
+    erase.onclick = function () {
+      erase.innerHTML = "";
   }
   */
-
+ /*
+ document.getElementById("reset").addEventListener("click", () =>{
+  clearAll();
+})
+*/
 /*
 let copyDiv = document.createElement("div"); //div to contain copy button
     let buton = document.createElement("button"); // copy button created
